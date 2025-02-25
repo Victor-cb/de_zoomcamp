b4fa8782dfd44959a1df93cce52011cc40004832
Victor Brito <vbrito.analytics@gmail.com>
2025-02-25 18:43:30 +0000

    Last Changes before lost access

--- models/core/fact_trips.sql
+++ models/core/fact_trips.sql
@@ -48,7 +48,11 @@ select trips_unioned.tripid,
     trips_unioned.improvement_surcharge, 
     trips_unioned.total_amount, 
     trips_unioned.payment_type, 
-    trips_unioned.payment_type_description
+    trips_unioned.payment_type_description,
+    EXTRACT(YEAR FROM trips_unioned.pickup_datetime ) as year,
+    EXTRACT(MONTH FROM trips_unioned.pickup_datetime) as month,
+    EXTRACT(QUARTER from trips_unioned.pickup_datetime) as quarter,
+    concat(EXTRACT( YEAR FROM trips_unioned.pickup_datetime ), '/', EXTRACT(QUARTER FROM trips_unioned.pickup_datetime)) as year_quarter
 from trips_unioned
 inner join dim_zones as pickup_zone 
 on trips_unioned.pickup_locationid = pickup_zone.locationid
